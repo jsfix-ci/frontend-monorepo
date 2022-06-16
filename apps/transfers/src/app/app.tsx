5405fb7f21b7a4dd8c004bc61a25b6e9873e2528
Dexter <dexter.edwards93@gmail.com>
2022-06-16 13:33:37 +0100

    feat: add theme switcher

--- apps/transfers/src/app/app.tsx
+++ apps/transfers/src/app/app.tsx
@@ -1,14 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { ThemeContext, useThemeSwitcher } from '@vegaprotocol/react-helpers';
-import { Button } from '@vegaprotocol/ui-toolkit';
+import { t, ThemeContext, useThemeSwitcher } from '@vegaprotocol/react-helpers';
+import { Button, ThemeSwitcher } from '@vegaprotocol/ui-toolkit';
 import {
-  // useVegaWallet,
+  useVegaWallet,
   VegaConnectDialog,
   VegaWalletProvider,
 } from '@vegaprotocol/wallet';
 import { RestConnector } from '@vegaprotocol/wallet';
-import type { ButtonHTMLAttributes } from 'react';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 export const rest = new RestConnector();
 
@@ -16,27 +15,35 @@ export const Connectors = {
   rest,
 };
 
-const ConnectToVegaWallet = (
-  props: ButtonHTMLAttributes<HTMLButtonElement>
-) => {
-  // const { keypair } = useVegaWallet();
-  // const isConnected = keypair !== null;
-
-  return <Button {...props}>Connect Vega Wallet</Button>;
+const Transfers = ({
+  setConnectModalShown,
+}: {
+  setConnectModalShown: (value: boolean) => void;
+}) => {
+  const { keypair } = useVegaWallet();
+  const isConnected = useMemo(() => keypair !== null, [keypair]);
+  return isConnected ? null : (
+    <Button onClick={() => setConnectModalShown(true)}>
+      Connect Vega Wallet
+    </Button>
+  );
 };
 
 export function App() {
   const [dialogOpen, setDialogOpen] = useState(true);
-  const [theme] = useThemeSwitcher();
+  const [theme, toggleTheme] = useThemeSwitcher();
   return (
     <div className="antialiased m-0 bg-white dark:bg-black text-black dark:text-white h-full">
       <ThemeContext.Provider value={theme}>
         <VegaWalletProvider>
-          <section>
-            <h1 className="uppercase calt mb-2 font-alpha text-h3">
-              Transfers
-            </h1>
-            <ConnectToVegaWallet onClick={() => setDialogOpen(true)} />
+          <section className="text-center pt-32">
+            <div className="flex justify-center mb-16">
+              <h1 className="uppercase calt mr-8 font-alpha text-h3">
+                {t('Transfers')}
+              </h1>
+              <ThemeSwitcher onToggle={toggleTheme} className="-my-4" />
+            </div>
+            <Transfers setConnectModalShown={setDialogOpen} />
             <VegaConnectDialog
               connectors={Connectors}
               dialogOpen={dialogOpen}
