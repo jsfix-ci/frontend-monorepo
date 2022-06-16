1bb8d01c328b33460402fc0d709b5f4b33227bc6
Dexter <dexter.edwards93@gmail.com>
2022-06-16 15:39:50 +0100

    feat: graphql for pulling assets, validation, select asset

---
+++ apps/transfers/src/app/__generated__/PartyAccounts.ts
@@ -0,0 +1,51 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: PartyAccounts
+// ====================================================
+
+export interface PartyAccounts_party_accounts_asset {
+  __typename: "Asset";
+  /**
+   * The id of the asset
+   */
+  id: string;
+  /**
+   * The full name of the asset (e.g: Great British Pound)
+   */
+  name: string;
+  /**
+   * The precision of the asset
+   */
+  decimals: number;
+}
+
+export interface PartyAccounts_party_accounts {
+  __typename: "Account";
+  /**
+   * Asset, the 'currency'
+   */
+  asset: PartyAccounts_party_accounts_asset;
+  /**
+   * Balance as string - current account balance (approx. as balances can be updated several times per second)
+   */
+  balance: string;
+}
+
+export interface PartyAccounts_party {
+  __typename: "Party";
+  /**
+   * Collateral accounts relating to a party
+   */
+  accounts: PartyAccounts_party_accounts[] | null;
+}
+
+export interface PartyAccounts {
+  /**
+   * An entity that is trading on the VEGA network
+   */
+  party: PartyAccounts_party | null;
+}
--- apps/transfers/src/app/app.tsx
+++ apps/transfers/src/app/app.tsx
@@ -1,9 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ApolloProvider, gql, useQuery } from '@apollo/client';
 import {
+  addDecimal,
+  maxSafe,
   required,
   t,
   ThemeContext,
   useThemeSwitcher,
+  vegaPublicKey,
 } from '@vegaprotocol/react-helpers';
 import {
   Button,
@@ -20,13 +23,38 @@ import {
   VegaWalletProvider,
 } from '@vegaprotocol/wallet';
 import { RestConnector } from '@vegaprotocol/wallet';
+import BigNumber from 'bignumber.js';
+import type { ReactNode } from 'react';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { createClient } from '../lib/apollo-client';
+import type {
+  PartyAccounts,
+  PartyAccounts_party_accounts,
+} from './__generated__/PartyAccounts';
 
 export const rest = new RestConnector();
 
 export const Connectors = {
   rest,
+};
+
+// TODO move to UI toolkit
+interface UseButtonProps {
+  children: ReactNode;
+  onClick: () => void;
+}
+
+const UseButton = ({ children, onClick }: UseButtonProps) => {
+  return (
+    <button
+      type="button"
+      className="ml-auto text-ui absolute top-0 right-0 underline"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 };
 
 const ACCOUNT_TYPES = [
@@ -131,6 +159,23 @@ const ACCOUNT_TYPES = [
   },
 ];
 
+const ACCOUNTS_QUERY = gql`
+  query PartyAccounts {
+    party(
+      id: "c443fb0388266c290736f325efeaad9aaa6d5f7f7b184f4e2302ecf8207b056e"
+    ) {
+      accounts {
+        asset {
+          id
+          name
+          decimals
+        }
+        balance
+      }
+    }
+  }
+`;
+
 interface FormFields {
   toAddress: string;
   amount: string;
@@ -138,47 +183,67 @@ interface FormFields {
   toAccountType: string;
 }
 
-const TransfersForm = () => {
-  const { register, handleSubmit } = useForm<FormFields>();
-  const [error, setError] = useState<Error | null>(null);
+const TransfersForm = ({
+  account,
+}: {
+  account: PartyAccounts_party_accounts;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<FormFields>();
   const onSubmit = useCallback((fields: FormFields) => {
-    setError(new Error(t("Something doesn't look right")));
+    console.log('send', fields);
   }, []);
+  const max = useMemo(
+    () => addDecimal(account.balance, account.asset.decimals).toString(),
+    [account.asset.decimals, account.balance]
+  );
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <FormGroup label={t('Amount:')} labelFor="toAddress">
+      <FormGroup label={t('Amount:')} labelFor="toAddress" className="relative">
         <Input
-          {...register('amount', { validate: { required } })}
+          {...register('amount', {
+            validate: {
+              required,
+              maxSafe: (value) => maxSafe(new BigNumber(max))(value),
+            },
+          })}
           id="amount"
-          hasError={Boolean(error?.message)}
+          hasError={Boolean(errors.amount?.message)}
           type="number"
           autoFocus={true}
           placeholder={t('10')}
         />
-        {error?.message && (
-          <InputError intent="danger">{error.message}</InputError>
+        {errors.amount?.message && (
+          <InputError intent="danger">{errors.amount?.message}</InputError>
         )}
+        <UseButton
+          onClick={() => {
+            setValue('amount', max);
+          }}
+        >
+          {t('Use maximum')}
+        </UseButton>
       </FormGroup>
       <FormGroup label={t('To (Vega Address):')} labelFor="toAddress">
         <Input
-          {...register('toAddress', { validate: { required } })}
+          {...register('toAddress', { validate: { required, vegaPublicKey } })}
           id="toAddress"
-          hasError={Boolean(error?.message)}
+          hasError={Boolean(errors.toAddress?.message)}
           type="text"
           autoFocus={true}
           placeholder={t(
             'To address e.g. c443fb0388266c290736f325efeaad9aaa6d5f7f7b184f4e2302ecf8207b056e'
           )}
         />
-        {error?.message && (
-          <InputError intent="danger">{error.message}</InputError>
+        {errors.toAddress?.message && (
+          <InputError intent="danger">{errors.toAddress?.message}</InputError>
         )}
       </FormGroup>
-      <FormGroup
-        label={t('From account type:')}
-        labelFor="fromAccountType"
-        className="relative"
-      >
+      <FormGroup label={t('From account type:')} labelFor="fromAccountType">
         <Select
           {...register('fromAccountType', { validate: { required } })}
           id="asset"
@@ -192,17 +257,13 @@ const TransfersForm = () => {
             </option>
           ))}
         </Select>
-        {error?.message && (
+        {errors.fromAccountType?.message && (
           <InputError intent="danger" className="mt-4" forInput="asset">
-            {error?.message}
+            {errors.fromAccountType?.message}
           </InputError>
         )}
       </FormGroup>
-      <FormGroup
-        label={t('To account type:')}
-        labelFor="toAccountType"
-        className="relative"
-      >
+      <FormGroup label={t('To account type:')} labelFor="toAccountType">
         <Select
           {...register('toAccountType', { validate: { required } })}
           id="asset"
@@ -216,15 +277,17 @@ const TransfersForm = () => {
             </option>
           ))}
         </Select>
-        {error?.message && (
+        {errors.toAccountType?.message && (
           <InputError intent="danger" className="mt-4" forInput="asset">
-            {error?.message}
+            {errors.toAccountType?.message}
           </InputError>
         )}
       </FormGroup>
-      <Button type="submit" variant="secondary">
-        {t('Send')}
-      </Button>
+      <div className="text-center">
+        <Button type="submit" variant="secondary">
+          {t('Send')}
+        </Button>
+      </div>
     </form>
   );
 };
@@ -235,6 +298,14 @@ const TransfersContainer = () => {
   const [config, setConfig] = useState<any | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
+  const [account, setAsset] = useState<
+    PartyAccounts_party_accounts | undefined
+  >(undefined);
+  const {
+    loading: accountsLoading,
+    error: accountsError,
+    data,
+  } = useQuery<PartyAccounts>(ACCOUNTS_QUERY);
   useEffect(() => {
     const run = async () => {
       try {
@@ -265,17 +336,38 @@ const TransfersContainer = () => {
       }
     };
   }, [connector]);
-  if (loading) {
+  if (loading || accountsLoading) {
     return <Loader />;
-  } else if (error) {
-    return <div>{error.message}</div>;
+  } else if (error || accountsError) {
+    return <div>{error?.message || accountsError?.message}</div>;
   }
+  // TODO add empty state for if party does not exist or party has no money
   return (
     <section>
       {/* TODO */}
       {/* Connected to network: {config.name}
-      Using host config: {JSON.stringify(config.api.grpc)} */}
-      <TransfersForm />
+      Using host config: {JSON.stringify(config.api.grpc)}
+      Using data node: {JSON.stringify(config.api.graphQl)} */}
+      <FormGroup label={t('Asset:')} labelFor="asset" className="relative">
+        <Select
+          value={account?.asset.id}
+          onChange={(e) =>
+            setAsset(
+              data?.party?.accounts?.find((a) => a.asset.id === e.target.value)
+            )
+          }
+        >
+          <option value={undefined}>{t('Please select an asset')}</option>
+          {data?.party?.accounts?.map((d) => {
+            return (
+              <option value={d.asset.id} key={d.asset.name}>
+                {d.asset.name}
+              </option>
+            );
+          })}
+        </Select>
+      </FormGroup>
+      {account && <TransfersForm account={account} />}
     </section>
   );
 };
@@ -290,34 +382,44 @@ const Transfers = ({
   return isConnected ? (
     <TransfersContainer />
   ) : (
-    <Button onClick={() => setConnectModalShown(true)}>
-      Connect Vega Wallet
-    </Button>
+    <div className="text-center">
+      <Button onClick={() => setConnectModalShown(true)}>
+        Connect Vega Wallet
+      </Button>
+    </div>
   );
 };
 
 export function App() {
   const [dialogOpen, setDialogOpen] = useState(true);
   const [theme, toggleTheme] = useThemeSwitcher();
+  // TODO this should be pulled from wallet config
+  const client = useMemo(
+    () => createClient('https://lb.testnet.vega.xyz/query'),
+    []
+  );
+
   return (
     <div className="antialiased m-0 h-full flex justify-center vega-bg">
       <ThemeContext.Provider value={theme}>
-        <VegaWalletProvider>
-          <section className="text-center pt-32 border-x-1 px-64 w-[400px] dark:bg-black bg-white text-black dark:text-white ">
-            <div className="flex justify-center mb-16">
-              <h1 className="uppercase calt mr-8 font-alpha text-h3">
-                {t('Transfers')}
-              </h1>
-              <ThemeSwitcher onToggle={toggleTheme} className="-my-4" />
-            </div>
-            <Transfers setConnectModalShown={setDialogOpen} />
-            <VegaConnectDialog
-              connectors={Connectors}
-              dialogOpen={dialogOpen}
-              setDialogOpen={setDialogOpen}
-            />
-          </section>
-        </VegaWalletProvider>
+        <ApolloProvider client={client}>
+          <VegaWalletProvider>
+            <section className="pt-32 border-x-1 px-64 w-[400px] dark:bg-black bg-white text-black dark:text-white ">
+              <div className="flex justify-center mb-16">
+                <h1 className="uppercase calt mr-8 font-alpha text-h3">
+                  {t('Transfers')}
+                </h1>
+                <ThemeSwitcher onToggle={toggleTheme} className="-my-4" />
+              </div>
+              <Transfers setConnectModalShown={setDialogOpen} />
+              <VegaConnectDialog
+                connectors={Connectors}
+                dialogOpen={dialogOpen}
+                setDialogOpen={setDialogOpen}
+              />
+            </section>
+          </VegaWalletProvider>
+        </ApolloProvider>
       </ThemeContext.Provider>
     </div>
   );
