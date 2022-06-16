218472cc0ccf39a07dc9caf95b44b84edc257e0b
Dexter <dexter.edwards93@gmail.com>
2022-06-16 12:23:56 +0100

    feat: scafold transfers app

---
+++ apps/transfers/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app/app';
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
